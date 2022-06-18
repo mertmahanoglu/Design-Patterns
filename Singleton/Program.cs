bc68fd3c2a66921e3b833182e9887e19b4bf7d8e
Mert <Mert@DESKTOP-318LSJH>
2022-06-18 22:49:40 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cant create new object
+            //ProductOperations productOperations = new ProductOperations();
+            var productOperations = ProductOperations.CreateAsSingleton();
+            productOperations.SaveProduct();
+        }
+    }
+
+    class ProductOperations
+    {
+        static ProductOperations _productOperations;
+
+        //Prevent to create new object 
+        private ProductOperations()
+        {
+
+        }
+
+        public static ProductOperations CreateAsSingleton()
+        {
+            return _productOperations ?? (_productOperations = new ProductOperations());
+        }
+
+        public void SaveProduct()
+        {
+            Console.WriteLine("Product Saved");
+        }
+
+    }
+}
